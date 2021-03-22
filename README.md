cd076f19b0b2ad966b89a8be4adf9d54abe0cd99
cconejov <cconejov@gmail.com>
2021-03-22 17:15:39 +0100

    Creation repo NPD Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NPS_Problems
 Problem Set Exercises Course NonParametric statistics
+
+**IMPORTANT** We are the groou 18
---
+++ R_Code/2-exercise2_19.R
@@ -0,0 +1,7 @@
+## --------------------------------------------------
+## Exercise 2: Compare the MISE and AMISE criteria in three densities in nor1mix of your choice.
+
+## 1. Code (2.33) and the AMISE expression for the normal kernel, and compare the two error curves.
+
+## 2. Compare them for n = 100, 200, 500, adding a vertical line to represent the $h_{MISE}$ and $h_{AMISE}$ bandwidths. Describe in detail the results and the major takeaways.
+## --------------------------------------------------
