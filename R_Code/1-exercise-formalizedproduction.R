2a6dc4e9c4ea0510fe757ac10fcbf6814d43d4ca
xavier414 <xavierbryant@shaw.ca>
2021-04-01 02:12:04 +0200

    final RMD changes

--- R_Code/1-exercise-formalizedproduction.R
+++ R_Code/1-exercise-formalizedproduction.R
@@ -36,14 +36,8 @@ summary(pastPhones)
 
 # Histograms - default bandwidths
 
-par(mfrow = c(1,2))
-p1 <- hist(problemPhones, xlim=c(0,65))
-p2 <- hist(pastPhones, xlim=c(0,65))
-par(mfrow = c(1,1))
-
-plot( p1, col=rgb(0,0,1,1/4), xlim=c(0,65), main = "Comparison Histograms")  # first histogram
-plot( p2, col=rgb(1,0,0,1/4), xlim=c(0,65), add=T)  
-rm(p1,p2)
+p1 <- hist(problemPhones, xlim=c(0,65), probability = T, col=rgb(1,0,0,1/4), main = "Problematic Phones")
+p2 <- hist(pastPhones, xlim=c(0,65), probability = T, col=rgb(0,0,1,1/4), main = "Past Phones")
 
 # Density plots - default bandwidths
 
@@ -62,13 +56,8 @@ par(mfrow = c(1,1))
 bw.nrd0(x = problemPhones)
 bw.nrd0(x = pastPhones)
 
-# TRANSFORMATIONS
 
-# Transformations must be performed in order to avoid boundary bias, which is to assign probability, 
-# with the function mapping, when there is no real density to support it.
-# Hence, we are looking at temperatures that theoretically can take values below zero or close to zero to  a very  high values that can be considered
-# positive infinity. In reality, in our sample, does not have any values close to zero, therefore boundary bias is a risk. From this, we can deduct that a log
-# transformation could be possible but we will check if this is necesaary in our sample. 
+# TRANSFORMATIONS
 
 # !Problematic temperatures =================================
 
@@ -121,27 +110,45 @@ kde_transf_pastPhones$bw
 ## Part b. ----
 ## Is there any important difference on the results from considering the LSCV selector over the DPI selector?
 
+# !Problematic temperatures =================================
 
-##    BANDWIDTH SELECTION
+#LSCV
 
-# We will use several techniques to select the optimal bandwidth we discussed over the term. These process are processes are the "rule of thumb" bandwidth selection 
-# derived from the combining from minimizing the AMISE with a normal parametric assumption for the unknown $R(f'')$, part of the AMISE (asympotic mean integrated
-# squared error)  derivation: $h_{AMISE} = [\frac{R(K)}{\mu^2_2(K)R(f'')n}]^\frac{1}{5}$. We then look at the Direct Plug In (DPI) selector, which instead of using a normal 
-# parameterization in the zero stage, the DPI buries the parametric assumption, usually, two stages between bias (lower with number of stages) and variance
-#(higher with the number of stages). We then look at two cross-validation techniques, where we attempt to minimize the MISE (mean integrated squared error),
-# through means of cross validation, or leave-one-out techniques. There are two types: the Least Squared Cross-Validation (or Unbiased Cross Validation) and the
-# Biased Cross Validation. Numerical optimization is required for the LSCV and therefore we can get trapped in spurious solutions, necessitating the limiting
-# of the bandwidth grid for the $h_{LSCV}$. The BCV adapts a hybrid strategy estimating a modification of $R(f'')$ with leave-out-diagonals. It is important that
-# the plot or the grid is reviewed in this selector as well as we are looking for the local minimizer not the global as h, the bandwidth length, goes to infinity.
-# The $\hat{h}_{LSCV}$ has more much more variance than the $\hat{h}_{BCV}$, this reduction in variance tends to come at the cost of the increased bias, making
-# $\hat{h}_{BCV}$ usually larger than $\hat{h}_{LSCV}$. 
+bw.lscv.problemtemps <- bw.ucv(x = problemPhones)
 
-# The four methods are as below:
+#DPI
 
-# RT
-# DPI
-# LSCV
-# BCV
+bw_dpi_problemtemps <- bw.SJ(x = problemPhones, method = "dpi")
+
+# !Past temperatures =================================
+
+#LSCV
+
+bw.lscv.pasttemps <- bw.ucv(x = pastPhones)
+
+#DPI
+
+bw_dpi_pasttemps <- bw.SJ(x = pastPhones, method = "dpi")
+
+
+#PLOTS
+
+# Problem Temperatures
+
+par(mfrow=c(1,2))
+plot(density(x = problemtemps, bw = bw.lscv.problemtemps), xlim = c(-5,65)) 
+plot(density(x = problemtemps, bw = bw_dpi_problemtemps), xlim = c(-5,65))
+par(mfrow=c(1,1))
+
+
+# Past Temperatures
+
+par(mfrow=c(1,2))
+plot(density(x = pasttemps, bw = bw.lscv.pasttemps), xlim = c(-5,65))
+plot(density(x = pasttemps, bw = bw_dpi_pasttemps), xlim = c(-5,65))
+par(mfrow=c(1,1))
+
+
 
 ## Part c. ----
 ## It seems that in temps-7 there is a secondary mode. Compute a kernel derivative estimation for temps-7 and temps-other using what you consider are the most adequate bandwidths.
@@ -149,7 +156,7 @@ kde_transf_pastPhones$bw
 # !Problematic temperatures =================================
 
 ## Detect Global mode
-pos_Max <- which.max(density(x = problemPhones)$y)
+posMax1 <- which.max(density(x = problemPhones)$y)
 
 ## Detect minima
 YY <- density(x = problemPhones)$y[density(x = problemPhones)$x > 20 & density(x = problemPhones)$x < 40]
@@ -159,7 +166,7 @@ posMin <- which(density(x = problemPhones)$y == minY)
 ## Detect Secondary mode
 YY <- density(x = problemPhones)$y[density(x = problemPhones)$x > 33 & density(x = problemPhones)$x < 60]
 maxY <- max(YY)
-posMax <- which(density(x = problemPhones)$y == maxY)
+posMax2 <- which(density(x = problemPhones)$y == maxY)
 
 
 kdde_0_problemPhones <- ks::kdde(x = problemPhones, deriv.order = 0)
@@ -167,8 +174,8 @@ kdde_1_problemPhones <- ks::kdde(x = problemPhones, deriv.order = 1)
 
 par(mfrow = c(1,2))
 plot(kdde_0_problemPhones, xlab = "x", main = "Problematic battery: Density estimation", xlim = c(-5,65))
-abline(v = density(x = problemPhones)$x[pos_Max], col = "3")
-abline(v = density(x = problemPhones)$x[posMax], col = "3")
+abline(v = density(x = problemPhones)$x[posMax1], col = "3")
+abline(v = density(x = problemPhones)$x[posMax2], col = "3")
 abline(v = density(x = problemPhones)$x[posMin], col = "2")
 plot(kdde_1_problemPhones, xlab = "x", main = "Problematic battery: Density derivative estimation")
 abline(v = density(x = problemPhones)$x[pos_Max], col = "3")
@@ -200,6 +207,21 @@ par(mfrow = c(1,1))
 ## Part d. ---- 
 ## Precisely determine the location of the extreme points.
 
+# !Problematic temperatures =================================
+
+density(x = problemPhones)$x[posMax1]  
+density(x = problemPhones)$x[posMax2]
+
+density(x = problemPhones)$y[posMax1]
+density(x = problemPhones)$y[posMax2]  
+
+# !Problematic temperatures =================================
+
+density(x = pastPhones)$y[posMax1]
+density(x = pastPhones)$y[posMax2]  
+
+density(x = pastPhones)$x[posMax1]  
+density(x = pastPhones)$x[posMax2]
 
 
 ## Part e. ---- 
@@ -210,17 +232,17 @@ kdde_2_problemPhones <- ks::kdde(x = problemPhones, deriv.order = 2)
 
 par(mfrow = c(1,3))
 plot(kdde_0_problemPhones, xlab = "x", main = "Problematic battery: \n Density estimation", xlim = c(-5,65))
-abline(v = density(x = problemPhones)$x[pos_Max], col = "3")
-abline(v = density(x = problemPhones)$x[posMax], col = "3")
+abline(v = density(x = problemPhones)$x[posMax1], col = "3")
+abline(v = density(x = problemPhones)$x[posMax2], col = "3")
 abline(v = density(x = problemPhones)$x[posMin], col = "2")
 plot(kdde_1_problemPhones, xlab = "x", main = "Problematic battery: \n Density derivative estimation")
-abline(v = density(x = problemPhones)$x[pos_Max], col = "3")
-abline(v = density(x = problemPhones)$x[posMax], col = "3")
+abline(v = density(x = problemPhones)$x[posMax1], col = "3")
+abline(v = density(x = problemPhones)$x[posMax2], col = "3")
 abline(v = density(x = problemPhones)$x[posMin], col = "2")
 abline(h = 0)
 plot(kdde_2_problemPhones, xlab = "x", main = "Problematic battery: \n  Density second derivative estimation")
-abline(v = density(x = problemPhones)$x[pos_Max], col = "3")
-abline(v = density(x = problemPhones)$x[posMax], col = "3")
+abline(v = density(x = problemPhones)$x[posMax1], col = "3")
+abline(v = density(x = problemPhones)$x[posMax2], col = "3")
 abline(v = density(x = problemPhones)$x[posMin], col = "2")
 abline(h = 0)
 par(mfrow = c(1,1))
